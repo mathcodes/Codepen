7d8e965aec9f1ddb2e324b40d96a293f9e28295d
mathcodes <jonpchristie@gmail.com>
2022-10-19 23:31:22 -0400

    massive sound set

---
+++ projects/drumkit/script.js
@@ -0,0 +1,20 @@
+
+      function playSound(e) {
+        const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+        const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+        if (!audio) return; // stop the function from running at all if no element is selected
+        audio.currentTime = 0; // rewinding the audio to it's starting position
+        audio.play();
+        key.classList.add("playing");
+      }
+
+      function removeTransition(e) {
+        if (e.propertyName !== "transform") return; // skip it if it's not a transform
+        this.classList.remove("playing");
+      }
+
+      const keys = document.querySelectorAll(".key");
+      keys.forEach(key =>
+        key.addEventListener("transitionend", removeTransition)
+      );
+      window.addEventListener("keydown", playSound);
